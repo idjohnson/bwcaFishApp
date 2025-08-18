cd405bf87ae0a9ae2ddeb681df6a48ec4ec8c919
Isaac Johnson <isaac.johnson@gmail.com>
2025-08-18 15:02:32 -0500

    add terraform updates, chart tweaks, cicd metric tweak

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,11 +18,17 @@ resource "google_artifact_registry_repository" "repo" {
   format        = "DOCKER"
 }
 
+resource "google_service_account" "service_account" {
+  account_id   = "${var.service_name}-sa"
+  display_name = "Service Account for ${var.service_name}"
+}
+
 resource "google_cloud_run_v2_service" "default" {
   name     = var.service_name
   location = var.region
 
   template {
+    service_account = google_service_account.service_account.email
     containers {
       image = "${google_artifact_registry_repository.repo.location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.repo.repository_id}/${var.service_name}:latest"
       ports {
@@ -75,6 +81,20 @@ resource "google_secret_manager_secret" "otel_api_key" {
   replication {
     auto {}
   }
+}
+
+resource "google_secret_manager_secret_iam_member" "otel_endpoint_accessor" {
+  project   = google_secret_manager_secret.otel_endpoint.project
+  secret_id = google_secret_manager_secret.otel_endpoint.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "otel_api_key_accessor" {
+  project   = google_secret_manager_secret.otel_api_key.project
+  secret_id = google_secret_manager_secret.otel_api_key.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_cloud_run_service_iam_member" "noauth" {
