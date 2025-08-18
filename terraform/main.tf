5aff1a3ab3cbea8cd031fa9512f93be023ffd3ce
Isaac Johnson <isaac.johnson@gmail.com>
2025-08-18 13:27:59 -0500

    packaging issue, tf with gsm

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,13 +28,53 @@ resource "google_cloud_run_v2_service" "default" {
       ports {
         container_port = 3000
       }
+
+      env {
+        name = "OTEL_EXPORTER_OTLP_ENDPOINT"
+        value_source {
+          secret_key_ref {
+            secret  = "OtelEndpoint"
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "OTEL_EXPORTER_OTLP_APIKEY"
+        value_source {
+          secret_key_ref {
+            secret  = "OtelApiKey"
+            version = "latest"
+          }
+        }
+      }
     }
   }
 
   depends_on = [
     google_project_service.run_api,
-    google_project_service.artifactregistry_api
+    google_project_service.artifactregistry_api,
+    google_project_service.secretmanager_api
   ]
+}
+
+resource "google_project_service" "secretmanager_api" {
+  service = "secretmanager.googleapis.com"
+}
+
+resource "google_secret_manager_secret" "otel_endpoint" {
+  secret_id = "OtelEndpoint"
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret" "otel_api_key" {
+  secret_id = "OtelApiKey"
+
+  replication {
+    auto {}
+  }
 }
 
 resource "google_cloud_run_service_iam_member" "noauth" {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,13 @@ output "service_url" {
   description = "The URL of the deployed Cloud Run service."
   value       = google_cloud_run_v2_service.default.uri
 }
+
+output "otel_endpoint_secret_id" {
+  description = "The ID of the OtelEndpoint secret."
+  value       = google_secret_manager_secret.otel_endpoint.secret_id
+}
+
+output "otel_api_key_secret_id" {
+  description = "The ID of the OtelApiKey secret."
+  value       = google_secret_manager_secret.otel_api_key.secret_id
+}
